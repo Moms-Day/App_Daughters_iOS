610deae673919ac4af817b8b4ed551a6b8c13984
Jeongdasilver <alal8272@gmail.com>
2019-02-07 18:05:31 +0900

    UPDATE InquiryContnetVC conditionCollectionView

--- MomsDayDaughter/MomsDayDaughter/VC/Main/Inquiry/InquiryContentVC.swift
+++ MomsDayDaughter/MomsDayDaughter/VC/Main/Inquiry/InquiryContentVC.swift
@@ -11,6 +11,7 @@ import UIKit
 class InquiryContentVC: UIViewController {
     
     var id: String!
+    let conditions = ["활동량 감소", "저체온", "고열","고혈압","저혈압","수면 부족","식욕 감퇴","폭식","설사","변비","구토","배뇨활동 불편","인지력 감퇴","빈혈","기침"]
     @IBOutlet weak var scheduleTable: dynamicTableView!
     @IBOutlet weak var dateLabel: UILabel!
     
@@ -52,4 +53,31 @@ extension InquiryContentVC: UITableViewDelegate, UITableViewDataSource {
 internal class scheduleDTO: UITableViewCell {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var scheduleLabel: UILabel!
+}
+
+extension InquiryContentVC: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return conditions.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "conditionCell", for: indexPath) as! conditionCell
+        cell.conditionLabel.text = conditions[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 30
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+internal class conditionCell: UICollectionViewCell {
+    @IBOutlet weak var conditionImage: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    
 }
