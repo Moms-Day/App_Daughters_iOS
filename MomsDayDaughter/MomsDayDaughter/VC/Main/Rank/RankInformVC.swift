56f51d4ff23e6e949aae9f6b495e66696e9bba94
Jeongdasilver <alal8272@gmail.com>
2019-03-02 22:54:05 +0900

    UPDATE RankInform

--- MomsDayDaughter/MomsDayDaughter/VC/Main/Rank/RankInformVC.swift
+++ MomsDayDaughter/MomsDayDaughter/VC/Main/Rank/RankInformVC.swift
@@ -10,8 +10,40 @@ import UIKit
 
 class RankInformVC: UIViewController {
 
+    @IBOutlet weak var hospitalScoreView: UIView!
+    @IBOutlet weak var careworkerScoreView: UIView!
+    @IBOutlet weak var reviewLabel: UILabel!
+    @IBOutlet weak var scoreViewContainer: UIView!
+    
+    var id: Bool = false //hospital: true, careowrker: false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if id {
+            hospitalScoreView.isHidden = false
+            careworkerScoreView.isHidden = true
+        } else {
+            hospitalScoreView.isHidden = true
+            careworkerScoreView.isHidden = false
+        }
     }
+}
+
+extension RankInformVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reviewCell") as! reviewDTO
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        return cell
+    }
+    
+    
+}
+
+internal class reviewDTO: UITableViewCell {
+    
 }
